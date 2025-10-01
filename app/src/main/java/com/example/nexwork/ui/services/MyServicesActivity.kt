ea64bfb41c010574da4c6ac48fc16512d24342cd
Inder Gutierrez <igutierrezsandoval42@gmail.com>
2025-10-01 10:18:10 -0500

    implment UI for Users crud

---
+++ app/src/main/java/com/example/nexwork/ui/services/MyServicesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nexwork.ui.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyServicesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: You will need to create a layout file for this activity
+        // For example: setContentView(R.layout.activity_my_services)
+    }
+}
